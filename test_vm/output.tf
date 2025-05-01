d80bbf819f9c75e1d11ad9a1f13a212f90e36185
Weznoe <45051798+Weznoe@users.noreply.github.com>
2025-05-01 21:33:58 +0000

    output the name of the test vm as well, and fix the instance type

--- test_vm/output.tf
+++ test_vm/output.tf
@@ -1,3 +1,8 @@
+output "name" {
+  value       = aws_instance.vm.tags["Name"]
+  description = "The name of the instance"
+}
+
 output "public_ip_address" {
   value       = aws_instance.vm.public_ip
   description = "The public IP address of the vm."
